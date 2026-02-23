b50681b425839bbde4d445d3f9b84da90dc51c4c
Martin2s4 <mulingemartin254@gmail.com>
2026-02-23 21:59:25 +0300

    we'll continue from there

--- main.py
+++ main.py
@@ -19,3 +19,16 @@ def get_users():
 def create_user(user: User):
     user.append(user)
     return {"message": "user created successfully", "user": user}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return user
+        return {"message": "user not found" }
+    
+
+@app.get("/search")
+def search_users(name: str):
+    results = [user for user in users if name.lower() in user.name.lower()]
+    return results
